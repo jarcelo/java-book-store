7a81f8601048561aad8c6383d0a4e5f41697cea3
Joseph Arcelo <josepharcelo@Josephs-MacBook-Air.local>
2016-12-02 12:56:59 -0600

    Modify store class to match database datatypes

--- src/java/business/Store.java
+++ src/java/business/Store.java
@@ -1,16 +1,66 @@
 
 package business;
 
-import java.math.BigDecimal;
-
 /**
  *
  * @author josepharcelo
  */
 public class Store
 {
-    private BigDecimal storeID;
+    private long storeID;
     private String storeName;
     private String storeAddress;
-    private BigDecimal numberOfStoreEmployees;
+    private long numberOfEmployees;
+    
+    public Store(){
+        storeID = 0;
+        storeName = "";
+        storeAddress = "";
+        numberOfEmployees = 0;
+    }
+    
+    public Store(long storeID, String storeName) {
+        this.storeID = storeID;
+        this.storeName = storeName;
+    }
+
+    public long getStoreID()
+    {
+        return storeID;
+    }
+
+    public void setStoreID(long storeID)
+    {
+        this.storeID = storeID;
+    }
+
+    public String getStoreName()
+    {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName)
+    {
+        this.storeName = storeName;
+    }
+
+    public String getStoreAddress()
+    {
+        return storeAddress;
+    }
+
+    public void setStoreAddress(String storeAddress)
+    {
+        this.storeAddress = storeAddress;
+    }
+
+    public long getNumberOfEmployees()
+    {
+        return numberOfEmployees;
+    }
+
+    public void setNumberOfEmployees(long numberOfEmployees)
+    {
+        this.numberOfEmployees = numberOfEmployees;
+    }
 }
