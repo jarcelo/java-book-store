0af89c94efd0dc8ef56e88a4adb6215f8e4dba50
Joe Arcelo <josepharcelo@gmail.com>
2016-12-15 00:40:37 -0600

    Add contructor and getters and setters for Book class

--- src/java/business/Book.java
+++ src/java/business/Book.java
@@ -1,8 +1,6 @@
 
 package business;
 
-import java.math.BigDecimal;
-
 /**
  *
  * @author josepharcelo
@@ -14,5 +12,90 @@ public class Book
     private String author;
     private String publisherCode;
     private String bookType;
-    private BigDecimal price;
+    //private long price;
+    private Double price;
+
+    public Book()
+    {
+        this.bookId = "";
+        this.title = "";
+        this.author = "";
+        this.publisherCode = "";
+        this.bookType = "";
+        this.price = 0.0;
+    }
+
+    public Book(String title, Double price)
+    {
+        this.title = title;
+        this.price = price;
+    }
+
+    
+    public String getBookId()
+    {
+        return bookId;
+    }
+
+    public void setBookId(String bookId)
+    {
+        this.bookId = bookId;
+    }
+
+    public String getTitle()
+    {
+        return title;
+    }
+
+    public void setTitle(String title)
+    {
+        this.title = title;
+    }
+
+    public String getAuthor()
+    {
+        return author;
+    }
+
+    public void setAuthor(String author)
+    {
+        this.author = author;
+    }
+
+    public String getPublisherCode()
+    {
+        return publisherCode;
+    }
+
+    public void setPublisherCode(String publisherCode)
+    {
+        this.publisherCode = publisherCode;
+    }
+
+    public String getBookType()
+    {
+        return bookType;
+    }
+
+    public void setBookType(String bookType)
+    {
+        this.bookType = bookType;
+    }
+
+    public Double getPrice()
+    {
+        return price;
+    }
+
+    public void setPrice(Double price)
+    {
+        this.price = price;
+    }
+    
+    @Override
+    public String toString(){
+        return String.format("Book Code: " + this.bookId + "<br>" +
+               "Book Title: " + this.title + "<br>" +
+               "Author: " + this.author);
+    }
 }
